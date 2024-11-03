a084dd089cb398bee42727c0a1ef4f652a0d3044
Abhishek <araj14250@gmail.com>
2024-11-04 00:01:19 +0530

    Almost Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,28 @@
         },
         d: function (e) {
             this._window = $(window),
-            this._document = $(document),
-            this._body = $('body'),
-            this._html = $('html')
-
+                this._document = $(document),
+                this._body = $('body'),
+                this._html = $('html')
         },
 
         methods: function (e) {
             imJs.featherAtcivation();
             imJs.backToTopInit();
+            imJs.mobileMenuActive();
             imJs.stickyHeader();
             imJs.smothScroll();
+            // imJs.contactForm();
+            imJs.wowActive();
             imJs.awsActivation();
-            
+        },
+
+        
+
+
+
+        wowActive: function () {
+            new WOW().init();
         },
 
         smothScroll: function () {
@@ -31,8 +40,6 @@
                 }, 300);
             });
         },
-        
-        
 
         featherAtcivation: function () {
             feather.replace()
@@ -53,7 +60,7 @@
                     $(scrollTop).css('opacity', '0');
                 }
             });
-            
+
             //Click event to scroll to top
             $(scrollTop).on('click', function () {
                 $('html, body').animate({
@@ -75,7 +82,55 @@
             })
         },
 
-        awsActivation:function(e){
+
+
+        mobileMenuActive: function (e) {
+            $('.humberger-menu').on('click', function (e) {
+                e.preventDefault();
+                $('.popup-mobile-menu').addClass('menu-open');
+                imJs._html.css({
+                    overflow: 'hidden'
+                })
+            });
+
+            $('.close-menu-activation, .popup-mobile-menu .primary-menu .nav-item a').on('click', function (e) {
+                e.preventDefault();
+                $('.popup-mobile-menu').removeClass('menu-open');
+                $('.has-droupdown > a').removeClass('open').siblings('.submenu').removeClass('active').slideUp('400');
+                imJs._html.css({
+                    overflow: ''
+                })
+            });
+
+            $('.popup-mobile-menu').on('click', function (e) {
+                e.target === this && $('.popup-mobile-menu').removeClass('menu-open');
+                imJs._html.css({
+                    overflow: ''
+                })
+            });
+
+
+            $('.has-droupdown > a').on('click', function (e) {
+                e.preventDefault();
+                $(this).siblings('.submenu').toggleClass('active').slideToggle('400');
+                $(this).toggleClass('open');
+                imJs._html.css({
+                    overflow: ''
+                })
+            });
+
+
+            $('.nav-pills .nav-link').on('click', function (e) {
+                $('.rn-popup-mobile-menu').removeClass('menu-open');
+                imJs._html.css({
+                    overflow: ''
+                })
+            })
+
+
+        },
+
+        awsActivation: function (e) {
             AOS.init();
         },
 
@@ -84,3 +139,61 @@
 
 
 })(jQuery, window)
+
+
+function qs (selector, all = false) {
+    return all ? document.querySelectorAll(selector) : document.querySelector(selector);
+  }
+  
+  const sections = qs('.timeline-section', true);
+  const timeline = qs('.timeline');
+  const line = qs('.timeline-line');
+  
+  line.style.bottom = `calc(100% - 20px)`;
+  let prevScrollY = window.scrollY;
+  let up, down;
+  let full = false;
+  let set = 0;
+  const targetY = window.innerHeight * .8;
+  
+  function scrollHandler(e) {
+    const {scrollY} = window;
+    
+    up = scrollY < prevScrollY;
+    down = !up;
+    
+    const timelineRect = timeline.getBoundingClientRect();
+    const lineRect = line.getBoundingClientRect();
+    // const lineHeight = lineRect.bottom - lineRect.top;
+    const dist = targetY - timelineRect.top;
+    
+    console.log(dist);
+    
+    if (down && !full) {
+      set = Math.max(set,dist);
+      line.style.bottom = `calc(100% - ${set}px)`;
+    }
+    
+    if (dist > timeline.offsetHeight + 50 && !full) {
+      full = true;
+      line.style.bottom = `-50px`;
+    }
+    
+    sections.forEach(item => {
+      // console.log(item);
+      const rect = item.getBoundingClientRect();
+  //     console.log(rect);
+      
+      if (rect.top + item.offsetHeight / 5 < targetY) {
+        item.classList.add('show-me');
+      }
+    });
+    
+    // console.log(up, down);
+    prevScrollY = window.scrollY;
+  }
+  
+  
+  scrollHandler();
+  line.style.display = 'block';
+  window.addEventListener('scroll', scrollHandler);
